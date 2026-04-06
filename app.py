ae3d9ccb76e01a2b5166c9e52967ba00db9b44e3
us4369564-web <us4369564@gmail.com>
2026-04-06 16:51:31 +0530

    Added Week 1 Flask app

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
+from flask import Flask
 
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Receipt Sharing Platform - Week 1 Setup Done"
+
+if __name__ == '__main__':
+    app.run()
